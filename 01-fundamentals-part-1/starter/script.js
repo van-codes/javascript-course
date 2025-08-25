b7fafebae0b7ffd0309f0278e4f2933f0d2376b1
van-codes <eyaaavan@gmail.com>
2025-08-25 04:37:31 -0500

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,30 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+// JavaScript Fundamentals - Hour 1
+// My first JavaScript code!
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+// Variables with LET (can be changed)
+// Useful for values that may change over time
+// Use it only when you need to change the value
+console.log("=== VARIABLES ===");
+
+let firstName = "Van";
+console.log(firstName);
+
+let age = 30;
+console.log(age);
+age = 20; // This will change the value of age
+console.log(age);
+
+// Variables with CONST (cannot be changed)
+const birthYear = 2005;
+console.log(birthYear);
+
+// birthYear = 1990; 
+// ^This will throw an error because birthYear is a constant
+
+const PI = 3.1415;
+console.log(PI);
+
+
